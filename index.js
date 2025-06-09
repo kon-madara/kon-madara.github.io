5d377dc32bbea5cffc8a8894101170f5dec190f1
kon-madara <docco_chan5945@icloud.com>
2025-06-10 00:04:49 +0900

    index.js を更新

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-あ
+const poems = document.querySelectorAll('.poem');
+let current = 0;
+
+setInterval(() => {
+  poems[current].classList.remove('active');
+  current = (current + 1) % poems.length;
+  poems[current].classList.add('active');
+}, 6000);
